6ece510e9af3316ab59d7277c6711e424d06c6ba
Wildan <wildanwigenta52@gmail.com>
2026-04-02 14:49:08 +0700

    add: User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,4 +29,13 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function user()
+{
+    return $this->belongsTo(User::class);
+}
+
+public function category()
+{
+    return $this->belongsTo(Category::class);
+}
 }
