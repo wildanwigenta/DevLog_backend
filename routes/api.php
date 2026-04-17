8c077f1e3dfd20f7dad9ac82d4a001321bb083a3
Wildan <wildanwigenta52@gmail.com>
2026-04-17 14:59:04 +0700

    feat: add routing

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\ArticleController;
 use App\Http\Controllers\API\CategoryController;
+use App\Http\Controllers\API\AuthController;
 
-Route::apiResource('articles', ArticleController::class);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('articles', ArticleController::class);
+});
+
 Route::apiResource('categories', CategoryController::class);
