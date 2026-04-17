a63787bcce5a70332df6b8002eb9d815987336f5
Wildan <wildanwigenta52@gmail.com>
2026-04-17 14:56:58 +0700

    feat: add logout controller

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Hash;
 use App\Http\Controllers\Controller;
 
 class AuthController extends Controller
-{   
+{
     public function register(Request $request)
     {
         $request->validate([
@@ -32,25 +32,33 @@ class AuthController extends Controller
     }
 
     public function login(Request $request)
-{
-    $request->validate([
-        'email' => 'required|email',
-        'password' => 'required'
-    ]);
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
 
-    $user = User::where('email', $request->email)->first();
+        $user = User::where('email', $request->email)->first();
 
-    if (!$user || !Hash::check($request->password, $user->password)) {
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'Email atau password salah'
+            ], 401);
+        }
+
+        $token = $user->createToken('api-token')->plainTextToken;
+
         return response()->json([
-            'message' => 'Email atau password salah'
-        ], 401);
+            'user' => $user,
+            'token' => $token
+        ]);
     }
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
 
-    $token = $user->createToken('api-token')->plainTextToken;
-
-    return response()->json([
-        'user' => $user,
-        'token' => $token
-    ]);
-}
+        return response()->json([
+            'message' => 'Logout berhasil'
+        ]);
+    }
 }
